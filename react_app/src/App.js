886b4bb4a289da0a7c7e3ef2afa0c93dba8aafb2
gangyeol kim <kkyy3402@naver.com>
2021-01-16 09:37:27 +0900

    기본 예제 추가중

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import {Container} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Container
+                className="mainTopView"
+            >
+                <Container
+                    className="navBar"
+                >
+                    <h>dd</h>
+
+                </Container>
+            </Container>
+
+        </div>
+    );
 }
 
 export default App;
